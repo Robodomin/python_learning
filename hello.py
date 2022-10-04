e931d0c6881d7be0778a0635c60ef8d4a4833625
Robodomin <dominik.panasz@outlook.com>
2022-10-04 11:44:42 +0200

    update

--- hello.py
+++ hello.py
@@ -1,4 +1,21 @@
-tewer = input("dajsesz ")
+import math
 
-
+tewer = "dajsesz "
+liczba = 0.0002
+lans = "masenwkw"
 print('mi nombre es Walter Blanco',tewer,end='.',sep=' bomba ')
+
+print(type(liczba))
+print(lans)
+lenscu = lans[::-1]
+print(lenscu)
+i = 0
+if liczba == 665:
+    print("ok")
+else:
+    print("fuck off")
+
+while (i<1000) :
+    liczba = liczba + math.sqrt(liczba);
+    print(i,"=",liczba," ")
+    i=i+1
